bab57707b9cc82b1bcae1a019ba13df1897dfc31
SwatxHero <swatxhero@gmail.com>
2019-03-25 23:03:25 -0500

    k

--- app/src/main/java/com/example/giba2/MainActivity.java
+++ app/src/main/java/com/example/giba2/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_event);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
---
+++ app/src/main/java/com/example/giba2/clock_in_out.java
@@ -0,0 +1,39 @@
+package com.example.giba2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Timer;
+
+public class clock_in_out extends AppCompatActivity {
+    private TextView timerText;
+    private Button startTimer;
+    private Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_in_out);
+        startTimer = (Button) findViewById(R.id.button13);
+        timerText = (TextView) findViewById(R.id.textView14);
+        startTimer.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd:MMMM:yyyy HH:mm:ss a");
+                String date = simpleDateFormat.format(calendar.getTime());
+                timerText.setText(date);
+            }
+
+
+
+        });
+    }
+
+}
